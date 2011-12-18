082dc3246831e9cf93767d455a91d3ae3c3c45a8
Sakujyo Wakuotsu <tomo3u3u@gmail.com>
2011-12-18 15:27:10 +0900

    分割コンパイルの例
    Signed-off-by: Sakujyo Wakuotsu <tomo3u3u@gmail.com>

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 CC	= gcc
 
-c1.exe	: c1.c
-	$(CC) -o c1.exe c1.c
+c1.exe	: c1.o myList.o
+	$(CC) -o c1.exe c1.o myList.o
+myList.o	: myList.h myList.c
+	$(CC) -c myList.c
+c1.o		: c1.c myList.h
+	$(CC) -c c1.c
 %.exe	: %.c
 	$(CC) -o $*.exe $*.c
 clean	:
 	rm c1.exe
+	rm c1.o
+	rm myList.o
---
+++ myList.h
@@ -0,0 +1,29 @@
+enum NodeType {
+	INT,
+	DOUBLE,
+	STRING
+};
+
+struct _Node {
+	enum NodeType type;
+	void *val;
+	struct _Node *next;
+};
+typedef struct _Node Node;
+
+struct _List {
+	int length;
+	Node *head;
+	Node *tail;
+};
+typedef struct _List List;
+
+Node *newNodeInt(int val);
+Node *newNodeDouble(double val);
+Node *newNodeString(char *s);
+void NodePrint(Node *n);
+void destructNode(Node *n);
+List *newList(void);
+void destructList(List *l);
+void ListAdd(List *l, Node *n);
+void ListPrint(List *l);
